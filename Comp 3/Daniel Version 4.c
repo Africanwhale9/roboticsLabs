2db5c51369ac99e09d77448d52502f7810391252
Matt Donnelly <cardinalsfan14@gmail.com>
2018-03-05 17:17:11 -0600

    Started V4 with my own changes

--- Comp 3/Daniel Version 4.c
+++ Comp 3/Daniel Version 4.c
@@ -1,18 +1,18 @@
 #pragma config(StandardModel, "EV3_REMBOT");
 
-int starting_point_x	=7;			// change to correct value
-int starting_point_y	=2;			// change to correct value
+int starting_point_x	=1;			// change to correct value
+int starting_point_y	=1;			// change to correct value
 
-int end_point_x	=2;						// change to correct value
-int end_point_y	=4;						// change to correct value
+int end_point_x	=4;						// change to correct value
+int end_point_y	=2;						// change to correct value
 
-int grid_size_x	=36;						// change to correct value
-int grid_size_y	=72;						// change to correct value
+int grid_size_x	=4;						// change to correct value
+int grid_size_y	=2;						// change to correct value
 
-float x_axis_y_intercepts_color	=25;		// change to correct value
-float y_axis_x_intercepts_color	=35;		// change to correct value
-float edege_color	=60;									// change to correct value
-float board_color	=100;									// change to correct value
+float x_axis_y_intercepts_color	=50;		// change to correct value
+float y_axis_x_intercepts_color	=6;		// change to correct value
+float edege_color	=95;									// change to correct value
+float board_color	=88;									// change to correct value
 
 int	global_lines_gone	=0;
 bool left_right;
@@ -21,9 +21,19 @@ bool edge;
 bool right;
 bool left;
 
+int motorSpeed = 13;
 
 
 
+bool outsideRange(float checkThis, float baseValue, float plus_minus){
+	if(checkThis>baseValue+plus_minus){
+		return true;
+	}
+	if(checkThis<baseValue-plus_minus){
+		return true;
+	}
+	return false;
+}
 
 
 
@@ -57,7 +67,7 @@ void turn(int degree){
 	ratio_finder();
 	startTask(monitor_deg_of_turn);
 	while (true){
-		setMotorSync(leftMotor, rightMotor, ratio, 10);
+		setMotorSync(leftMotor, rightMotor, ratio, motorSpeed);
 		if(degree_turned_so_far>= degree){
 			setMotorSync(leftMotor, rightMotor, 0, 0);
 			stopTask(monitor_deg_of_turn);
@@ -76,12 +86,14 @@ void turn(int degree){
 // go past X lines
 void go(int lines_need_to_go){
 	float current_color = getColorReflected(colorSensor);
-	while (global_lines_gone!= lines_need_to_go){//this should be <=, currently it will stop one box before. It needs to detect the far line of the box, not the close line
+	while (global_lines_gone<= lines_need_to_go){//this should be <=, currently it will stop one box before. It needs to detect the far line of the box, not the close line
 		current_color = getColorReflected(colorSensor);
-		setMotorSync(leftMotor,rightMotor,0,10);
-		if((!(current_color<= board_color+3)) && (!(current_color>= board_color-3))){ // this conditional is very confusing, make this into a function
+		setMotorSync(leftMotor,rightMotor,0,motorSpeed);
+		if(outsideRange(current_color, board_color, 8)){ // this conditional is very confusing, make this into a function
 			global_lines_gone++;
-			sleep(500);
+			if(global_lines_gone<= lines_need_to_go){//if it hasn't gone enough lines it'll sleep so it gets off the line. Otherwise, it should stop on the line
+				sleep(500);
+			}
 		}
 	}
 	return;
@@ -113,7 +125,8 @@ void get_color(){
 
 // to turn allong the x axis
 void face_with_x(){
-	get_color();
+	get_color(); //COMMENTED OUT FOR TESTING, ASSUME THAT IT IS FACING DOWN X AXIS
+	//left_right = true;//THIS IS ADDED FOR TESTING
 	if(up_down){
 		turn(90);
 	}
@@ -121,7 +134,7 @@ void face_with_x(){
 }
 
 //goes to where we think the edge is
-void go_to_edge(){
+void go_to_edge(){//no idea what this function is doing
 	int need_to_go;
 	if(starting_point_x< grid_size_x-starting_point_x){
 		need_to_go=starting_point_x;
@@ -131,7 +144,7 @@ void go_to_edge(){
 		need_to_go= grid_size_x-starting_point_x;
 		right=true;
 	}
-	go(need_to_go);
+	go(need_to_go);//how does it know what direction to go here? Wouldn't it need to turn?
 }
 
 //checks if this as an edge
@@ -145,17 +158,24 @@ void check_if_edge(){
 	}
 }
 
+task display(){
+	while(true){
+		displayBigTextLine(3, "lines gone: %d", global_lines_gone);
+	}
+}
+
 
 task main(){
+	startTask(display, 7);
 	go(1);
-	face_with_x();
+	face_with_x();//makes it face down the x axis
 	go_to_edge();
 	check_if_edge();
 	if(right){
 		turn(180);
 		int goes=grid_size_x-end_point_x;
 		go(goes);
-		if(end_point_x>starting_point_x){
+		if(end_point_y>starting_point_y){//shouldn't these be y's?
 			turn(90);
 			go(end_point_y-starting_point_y);
 		}
@@ -168,7 +188,7 @@ task main(){
 		turn(180);
 		int goes=end_point_x;
 		go(goes);
-		if(end_point_x>starting_point_x){
+		if(end_point_y>starting_point_y){//here also
 			turn(-90);
 			go(end_point_y-starting_point_y);
 		}
